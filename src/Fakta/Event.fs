023d15681b9feb65fe742b5f5b1400540bb709b9
Henrik Feldt <henrik@haf.se>
2015-05-23 23:48:53 +0200

    [Event, Health] API structures

--- src/Fakta/Event.fs
+++ src/Fakta/Event.fs
@@ -1,2 +1,15 @@
 ﻿module Fakta.Event
-()
+
+open System
+
+/// Fire is used to fire a new user event. Only the Name, Payload and Filters are respected. This returns the ID or an associated error. Cross DC requests are supported.
+let fire (state : FaktaState) (events : UserEvent list) (opts : WriteOptions) : Async<Choice<string * WriteMeta, Error>> =
+  raise (TBD "not used")
+
+/// IDToIndex is a bit of a hack. This simulates the index generation to convert an event ID into a WaitIndex.
+let idToIndex (state : FaktaState) (uuid : Guid) : uint64 =
+  raise (TBD "not used")
+
+/// List is used to get the most recent events an agent has received. This list can be optionally filtered by the name. This endpoint supports quasi-blocking queries. The index is not monotonic, nor does it provide provide LastContact or KnownLeader. 
+let list (name : string) (opts : QueryOptions) : Async<Choice<UserEvent list * QueryMeta, Error>> =
+  raise (TBD "not used")
--- src/Fakta/Health.fs
+++ src/Fakta/Health.fs
@@ -1,2 +1,19 @@
 ﻿module Fakta.Health
-()
+
+/// Checks is used to return the checks associated with a service
+let checks (state : FaktaState) (service : string) (opts : QueryOptions) : Async<Choice<HealthCheck list * QueryMeta, Error>> =
+  raise (TBD "not used")
+
+/// Node is used to query for checks belonging to a given node
+let node (state : FaktaState) (node : string) (opts : QueryOptions) : Async<Choice<HealthCheck list * QueryMeta, Error>> =
+  raise (TBD "not used")
+
+/// Service is used to query health information along with service info for a given service. It can optionally do server-side filtering on a tag or nodes with passing health checks only.
+let service (state : FaktaState) (service : string) (tag : string)
+            (passingOnly : bool) (opts : QueryOptions)
+            : Async<Choice<ServiceEntry list * QueryMeta, Error>> =
+  raise (TBD "not used")
+
+let state (state : FaktaState) (statee : string) (opts : QueryOptions) : Async<Choice<ServiceEntry list * QueryMeta, Error>> =
+  raise (TBD "not used")
+  
