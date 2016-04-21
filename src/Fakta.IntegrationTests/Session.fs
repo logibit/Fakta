12971e85c9aeab3d9f4ec052305d02a6cfbb6668
Zaneta Filipovska <leia@bofh.cz>
2016-04-21 14:09:19 +0200

    Session - wip

--- src/Fakta.IntegrationTests/Session.fs
+++ src/Fakta.IntegrationTests/Session.fs
@@ -14,5 +14,13 @@ let tests =
       let ttl = TTL (Duration.FromSeconds 10L)
       //let state = { state with config = { state.config with serverBaseUri = Uri "http://127.0.0.1:8501" } }
       let sessionId, writeMeta = ensureSuccess (Session.create state [ttl] []) id
+      let entry = ensureSuccess (Session.info state sessionId []) id
+      //let l = ensureSuccess (Session.renew state sessionId []) id
+      
+      let l = ensureSuccess (Session.renewPeriodic state (Duration.FromSeconds 10L) sessionId [] (Duration.FromSeconds 30L)) id
+      let entryList = ensureSuccess (Session.list state []) id
+      let list = ensureSuccess (Session.node state "COMP05" []) id
+      let l, m = ensureSuccess (Session.createNoChecks state [ttl] []) id
       ensureSuccess (Session.destroy state sessionId []) <| fun writeMeta -> ()
+      
   ]
