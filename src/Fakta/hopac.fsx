95cda4fefd2c1b19511340d826323e7db5e254ee
David Podhola <david.podhola@naseukoly.cz>
2016-04-25 20:10:46 +0200

    fixing https://gitlab.com/naseukolycz/Fakta/issues/3

--- src/Fakta/hopac.fsx
+++ src/Fakta/hopac.fsx
@@ -1,22 +1,22 @@
-//#I @"D:\codes\Fakta\src\packages\Hopac.0.1.3\lib\net45"
-//#I @"D:\codes\Fakta\src\packages\Hopac.Extras.0.1.33\lib\net45"
-//
-//#r "Hopac.Core.dll"
-//#r "Hopac.dll"
-//#r "Hopac.Platform.dll"
-//#r "Hopac.Extras.dll"
-//
-//open System
-//open Hopac.Extensions
-//open Hopac.Extras
-//open Hopac.Timer.Global
-//open Hopac.Infixes
-//open Hopac
-//
-//type Request<'a> =
-//  | Get
-//  | Put of 'a
-(*
+#I @"../../packages/Hopac/lib/net45"
+#I @"../../packages/Hopac.Extras/lib/net45"
+
+#r "Hopac.Core.dll"
+#r "Hopac.dll"
+#r "Hopac.Platform.dll"
+#r "Hopac.Extras.dll"
+
+open System
+open Hopac.Extensions
+open Hopac.Extras
+open Hopac.Timer.Global
+open Hopac.Infixes
+open Hopac
+
+type Request<'a> =
+  | Get
+  | Put of 'a
+
 type Cell<'a> = {
   reqCh: Ch<Request<'a>>
   replyCh: Ch<'a>
@@ -45,4 +45,3 @@ let cell (x: 'a) : Job<Cell<'a>> = job {
   do! Job.start (server x)
   return c
 }
-*)
