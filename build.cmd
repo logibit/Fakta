cf59ef8c1f30d2eca69d9ba11895cf5e96471820
David Podhola <david.podhola@naseukoly.cz>
2016-06-02 17:21:05 +0200

    fail the build if the bundle exec rake testing fails

--- build.cmd
+++ build.cmd
@@ -3,4 +3,7 @@ cd tools
 cmd /c start consul agent -dev -bind 127.0.0.1 -config-file=server.json
 cd ..
 call bundle exec rake
+set testerrorlevel=%errorlevel%
 taskkill /IM consul.exe
+rem echo %testerrorlevel%
+if %testerrorlevel% neq 0 exit /b %testerrorlevel%
