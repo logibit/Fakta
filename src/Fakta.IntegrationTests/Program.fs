1b8b408cc16028ce47f0a0baa4868683db37452e
David Podhola <david.podhola@naseukoly.cz>
2016-06-02 14:34:04 +0200

    run all tests

--- src/Fakta.IntegrationTests/Program.fs
+++ src/Fakta.IntegrationTests/Program.fs
@@ -4,10 +4,10 @@ open Fuchu
 
 [<EntryPoint>]
 let main argv = 
-  //Tests.defaultMainThisAssembly argv
+  Tests.defaultMainThisAssembly argv
   ///Tests.run Agent.tests
   //Tests.run ACL.tests
-  Tests.run Catalog.tests
+  //Tests.run Catalog.tests
   //Tests.run Event.tests
   //Tests.run Health.tests
   //Tests.run KV.tests
