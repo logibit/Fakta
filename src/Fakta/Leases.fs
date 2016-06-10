6775cf9ad4a155fc357107624868eb724dd01bbf
Zaneta Filipovska <leia@bofh.cz>
2016-06-10 17:19:16 +0200

    leases and secret - wip

--- src/Fakta/Leases.fs
+++ src/Fakta/Leases.fs
@@ -1,2 +1,78 @@
 ﻿module Fakta.Vault.Leases
 
+open Fakta
+open Fakta.Logging
+open Fakta.Impl
+open System
+open System.Collections
+open NodaTime
+open HttpFs.Client
+open Chiron
+open Hopac
+
+
+let leasesPath (funcName: string) =
+  [| "Fakta"; "Vault"; "Sys"; funcName |]
+
+let queryFilters state =
+  leasesPath >> queryFiltersNoMeta state
+
+let writeFilters state =
+  leasesPath >> writeFilters state
+
+let Renew state: WriteCallNoMeta<(string*int), Secret> =
+  let createRequest ((id, increment), opts) =
+    writeCallEntityUri state.config "sys/renew" (id, opts)
+    |> basicRequest state.config Put 
+    |> withVaultHeader state.config
+    |> withJsonBodyT (Map.empty.Add("increment", increment))
+    
+
+  let filters =
+    writeFilters state "renew"
+    >> respBodyFilter
+    >> codec createRequest fstOfJsonNoMeta
+
+  HttpFs.Client.getResponse |> filters
+
+let Revoke state: WriteCallNoMeta<string, unit> =
+  let createRequest (id, opts) =
+    writeCallEntityUri state.config "sys/renew" (id, opts)
+    |> basicRequest state.config Put 
+    |> withVaultHeader state.config
+    
+
+  let filters =
+    writeFilters state "revoke"
+    >> respBodyFilter
+    >> codec createRequest hasNoRespBody
+
+  HttpFs.Client.getResponse |> filters
+
+let RevokePrefix state: WriteCallNoMeta<string, unit> =
+  let createRequest (id, opts) =
+    writeCallEntityUri state.config "sys/revoke-prefix" (id, opts)
+    |> basicRequest state.config Put 
+    |> withVaultHeader state.config
+    
+
+  let filters =
+    writeFilters state "revoke"
+    >> respBodyFilter
+    >> codec createRequest hasNoRespBody
+
+  HttpFs.Client.getResponse |> filters
+
+let RevokeForce state: WriteCallNoMeta<string, unit> =
+  let createRequest (id, opts) =
+    writeCallEntityUri state.config "sys/revoke-force" (id, opts)
+    |> basicRequest state.config Put 
+    |> withVaultHeader state.config
+    
+
+  let filters =
+    writeFilters state "revoke-force"
+    >> respBodyFilter
+    >> codec createRequest hasNoRespBody
+
+  HttpFs.Client.getResponse |> filters
--- src/Fakta/Secret.fs
+++ src/Fakta/Secret.fs
@@ -1,2 +1,73 @@
 ﻿module Fakta.Vault.Secret
 
+open Fakta
+open Fakta.Logging
+open Fakta.Impl
+open System
+open System.Collections
+open NodaTime
+open HttpFs.Client
+open Chiron
+open Hopac
+
+
+let secretPath (funcName: string) =
+  [| "Fakta"; "Vault"; "Secret"; funcName |]
+
+let queryFilters state =
+  secretPath >> queryFiltersNoMeta state
+
+let writeFilters state =
+  secretPath >> writeFilters state
+
+
+let Read state: QueryCallNoMeta<string, Secret> =
+  let createRequest (path, opts) =
+    queryCall state.config path opts
+    |> withVaultHeader state.config
+
+  let filters =
+    queryFilters state "secretRead"
+    >> codec createRequest fstOfJsonNoMeta
+
+  HttpFs.Client.getResponse |> filters
+
+let List state: QueryCallNoMeta<string, Secret> =
+  let createRequest (path, opts) =
+    queryCall state.config path opts
+    |> withVaultHeader state.config
+    |> Request.queryStringItem "list" "true"
+
+  let filters =
+    queryFilters state "secretList"
+    >> codec createRequest fstOfJsonNoMeta
+
+  HttpFs.Client.getResponse |> filters
+
+let Write state: WriteCallNoMeta<string, unit> =     
+  let createRequest (path, opts) =
+    writeCallUri state.config path opts
+    |> basicRequest state.config Put 
+    |> withVaultHeader state.config
+    //|> withJsonBodyT json    
+
+  let filters =
+    writeFilters state "secretWrite"
+    >> respBodyFilter
+    >> codec createRequest hasNoRespBody
+
+  HttpFs.Client.getResponse |> filters
+
+let Delete state: WriteCallNoMeta<string, unit> =     
+  let createRequest (path, opts) =
+    writeCallUri state.config path opts
+    |> basicRequest state.config Delete 
+    |> withVaultHeader state.config
+    //|> withJsonBodyT json    
+
+  let filters =
+    writeFilters state "secretWrite"
+    >> respBodyFilter
+    >> codec createRequest hasNoRespBody
+
+  HttpFs.Client.getResponse |> filters
