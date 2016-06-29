b697cedf613fadbc773094bcc0e92b87bb7ec7a2
mavnn <michael@mavnn.co.uk>
2016-06-29 11:38:59 +0100

    [infra] Make testing easier on OSX

---
+++ tools/vault.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ ! -f vault ]]; then
+  version=0.6.0
+  file=vault_${version}_darwin_amd64.zip
+  curl --silent -L https://releases.hashicorp.com/vault/${version}/${file} -o vault.zip
+  unzip vault.zip
+fi
+
+export GOMAXPROCS=2
+exec ./vault "$@"
