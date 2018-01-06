fd878f2130ecd54c87ddafc10b016a9ba94dba77
Henrik Feldt <henrik@haf.se>
2018-01-06 17:46:00 +0100

    [tests] Fuchu -> Expecto (2)

--- src/Fakta.Tests/KV.fs
+++ src/Fakta.Tests/KV.fs
@@ -1,16 +1,19 @@
 ﻿module Fakta.Tests.KV
 
 open Expecto
-
+open Expecto.Flip
 open Fakta
 
 [<Tests>]
 let tests =
   testList "basics" [
-    testCase "format key value" <| fun _ ->
-      Assert.Equal("should be", "/v1/kv/a", Impl.keyFor "kv" "a")
-      Assert.Equal("should be", "/v1/kv/", Impl.keyFor "kv" "")
-      Assert.Equal("should be", "/v1/kv/", Impl.keyFor "kv" "/")
-      Assert.Equal("should be", "/v1/kv/a/b", Impl.keyFor "kv" "a/b")
-      Assert.Equal("should be", "/v1/kv/a/b/", Impl.keyFor "kv" "a/b/")
+    for expected, actual in
+      [ "/v1/kv/a", Impl.keyFor "kv" "a"
+        "/v1/kv/", Impl.keyFor "kv" ""
+        "/v1/kv/", Impl.keyFor "kv" "/"
+        "/v1/kv/a/b", Impl.keyFor "kv" "a/b"
+        "/v1/kv/a/b/", Impl.keyFor "kv" "a/b/"
+      ] ->
+      testCase (sprintf "format key value %s" expected) <| fun _ ->
+        actual |> Expect.equal "Should format path" expected
   ]
