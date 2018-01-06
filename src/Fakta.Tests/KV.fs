8dfb55046c0df432feaa6343b7568847c5a221cc
Henrik Feldt <henrik@haf.se>
2018-01-06 19:32:23 +0100

    [infra] improve nuget mgmt

--- src/Fakta.Tests/KV.fs
+++ src/Fakta.Tests/KV.fs
@@ -7,13 +7,13 @@ open Fakta
 [<Tests>]
 let tests =
   testList "basics" [
-    for expected, actual in
-      [ "/v1/kv/a", Impl.keyFor "kv" "a"
-        "/v1/kv/", Impl.keyFor "kv" ""
-        "/v1/kv/", Impl.keyFor "kv" "/"
-        "/v1/kv/a/b", Impl.keyFor "kv" "a/b"
-        "/v1/kv/a/b/", Impl.keyFor "kv" "a/b/"
+    for expected, actual, name in
+      [ "/v1/kv/a", Impl.keyFor "kv" "a", "a"
+        "/v1/kv/", Impl.keyFor "kv" "", "empty string"
+        "/v1/kv/", Impl.keyFor "kv" "/", "slash"
+        "/v1/kv/a/b", Impl.keyFor "kv" "a/b", "hiera, no trailing slash"
+        "/v1/kv/a/b/", Impl.keyFor "kv" "a/b/", "hiera, trailing slash"
       ] ->
-      testCase (sprintf "format key value %s" expected) <| fun _ ->
+      testCase (sprintf "format key value — %s" name) <| fun _ ->
         actual |> Expect.equal "Should format path" expected
   ]
